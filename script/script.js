eb3a6c177d7d75ef42370de751439480e11b0cea
Asbjørn <asbjorn.lindholm@hotmail.com>
2017-02-09 20:02:55 +0100

    My changes

---
+++ script/script.js
@@ -0,0 +1,31 @@
+
+
+$(document).ready(function() {
+
+    // Smooth Scroll
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+          }, 500);
+            return false;
+          }
+        }
+      });
+    });
+
+    // Read more
+    $('.read-more-toggle').on('click', function() {
+
+        console.log($(this).parent());
+
+        $(this).parent().find('.read-more').slideToggle();
+        $(this).hide();
+    });
+
+
+});
