9f42fbb1f94c5edd5df18ddb3f100310167a1b9e
Asbjørn <asbjorn.lindholm@hotmail.com>
2017-02-14 15:17:09 +0100

    v2

--- script/script.js
+++ script/script.js
@@ -18,12 +18,72 @@ $(document).ready(function() {
       });
     });
 
-    // Read more
-    $('.read-more-toggle').on('click', function() {
+    // Read More Toggle
+    $(function() {
+        // Read more
+        $('.read-more-toggle').on('click', function() {
 
-        $(this).parent().parent().find('.read-more').toggle();
-        $(this).hide();
+            var $textBlock = $(this).parent().parent().find('.read-more');
+            var $button = $(this);
+
+            $textBlock.toggleClass('visible');
+            $button.toggleClass('open');
+
+            if ($textBlock.hasClass('visible')) {
+                $button.html('Read Less');
+            } else {
+                $button.html('Read More');
+            }
+
+        });
     });
+
+    // Scroll Nav
+    $(function() {
+
+
+        var MQL = 1170;
+        var $window = $(window);
+        var $navBar = $('.navbar-custom');
+
+        if ($window.width() > MQL) {
+            $window.on('scroll', function() {
+                if ($window.scrollTop() > 0) {
+                    $navBar.addClass('is-visible is-fixed');
+                } else {
+                    $navBar.removeClass('is-visible is-fixed');
+                }
+            });
+        }
+        // if ($(window).width() > MQL) {
+
+            // var headerHeight = $('.navbar-custom').height();
+            // $(window).on('scroll', {
+            //         previousTop: 0
+            //     },
+            //     function() {
+            //         var currentTop = $(window).scrollTop();
+            //         //check if user is scrolling up
+            //         if (currentTop < this.previousTop) {
+            //             //if scrolling up...
+            //             if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+            //                 $('.navbar-custom').addClass('is-visible');
+            //             } else {
+            //                 $('.navbar-custom').removeClass('is-visible is-fixed');
+            //             }
+            //         } else if (currentTop > this.previousTop) {
+            //             //if scrolling down...
+            //             $('.navbar-custom').removeClass('is-visible');
+            //             if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+            //         }
+            //         this.previousTop = currentTop;
+            //     });
+        // }
+
+
+    });
+
+
 
 
 });
